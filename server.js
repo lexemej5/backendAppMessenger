cd78045aba15565691de44fd5c3e59e4deae53a3
lexemej5 <lexemej@gmail.com>
2025-11-23 18:02:52 +0100

    app-msg

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const connectDb = require("./config/db")
-const router = require("./routes/route")
+// server.js (or app.js)
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./config/db");
+const router = require("./routes/route");
 
-const PORT = process.env.PORT || 5000
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 connectDb();
+app.use("/api", router);
 
-app.use("/api", router)
-
-app.listen(PORT, ()=>{
-console.log(`server is running on port ${PORT}`)
-})
-
-
-
+module.exports = app;  // Export the app
